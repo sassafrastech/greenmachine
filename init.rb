72892cad55703a63896d4a589713f9e1fbf832ef
Tom Smyth <tom@sassafras.coop>
2015-03-29 23:02:47 -0400

    Added plugin structure.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :green_machine do
+  name 'Sassafras Green Machine'
+  author 'Tom Smyth'
+  description 'Sassafras Billing Plugin'
+  version '0.0.1'
+  url 'https://github.com/sassafrastech/greenmachine'
+  author_url 'http://sassafras.coop/about'
+end
