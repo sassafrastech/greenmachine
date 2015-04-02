872a87c5e838c22005e228ecad9b7e1dde75e483
Tom Smyth <tom@sassafras.coop>
2015-04-02 13:09:04 -0400

    Invoice number auto increment.

--- app/models/gm_invoice_creator.rb
+++ app/models/gm_invoice_creator.rb
@@ -1,7 +1,8 @@
 class GmInvoiceCreator
   ITEM_IDS = {development: 17}
+  INVOICE_NUM_LENGTH = 5
 
-  attr_accessor :report, :access_token, :token, :secret, :company_id
+  attr_accessor :report, :access_token, :token, :secret, :company_id, :service
 
   delegate :project, :interval, to: :report
 
@@ -12,11 +13,13 @@ class GmInvoiceCreator
   end
 
   def create
+    create_service
+
     # Given a Customer with ID=99 lets invoice them for an Item with ID=500
     invoice = Quickbooks::Model::Invoice.new
     invoice.customer_id = 97
     invoice.txn_date = Date.today
-    invoice.doc_number = '' # Autogenerate
+    invoice.doc_number = next_number
 
     report.totals.each do |user, hours|
       rate = user == :sassy ? project.gm_full_rate(interval).val : user.gm_project_rate(user, interval).val
@@ -33,9 +36,20 @@ class GmInvoiceCreator
       invoice.line_items << line_item
     end
 
-    service = Quickbooks::Service::Invoice.new
+    service.create(invoice)
+  end
+
+  private
+
+  def create_service
+    self.service = Quickbooks::Service::Invoice.new
     service.company_id = company_id
     service.access_token = access_token
-    service.create(invoice)
+  end
+
+  def next_number
+    existing = service.query('SELECT Id, DocNumber FROM Invoice ORDER BY MetaData.CreateTime DESC')
+    latest_num = existing.entries.first.doc_number
+    (latest_num.to_i + 1).to_s.rjust(INVOICE_NUM_LENGTH, '0')
   end
 end
