fb2192e3201376a002d78493d3255615ceda3c4c
Tom Smyth <tom@sassafras.coop>
2015-11-16 17:11:17 -0500

    Added rate table migration

---
+++ db/migrate/008_adjust_rates_table.rb
@@ -0,0 +1,9 @@
+class AdjustRatesTable < ActiveRecord::Migration
+  def up
+    add_column(:gm_rates, :user_type, :string)
+    add_index(:gm_rates, :user_type)
+    execute("UPDATE gm_rates SET
+      user_type = IF(kind = 'member_wage_base', 'member', NULL),
+      kind = IF(kind LIKE '%wage%', 'wage', IF(kind LIKE '%revenue%', 'revenue', kind))")
+  end
+end
