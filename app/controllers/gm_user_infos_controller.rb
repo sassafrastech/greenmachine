33db54f64fb201cd4321fae735cc58bb6e97066e
Adam Konner <adam.konner@gmail.com>
2016-01-10 22:31:09 -0500

    info > infos
    (caused other problems, not worth it)

--- app/controllers/gm_user_infos_controller.rb
+++ app/controllers/gm_user_infos_controller.rb
@@ -1,27 +1,27 @@
-class GmUserInfoController < GmApplicationController
+class GmUserInfosController < GmApplicationController
   unloadable
   before_filter :authorize
   before_action :set_gm_user_info, only: [:show, :edit, :update, :destroy]
 
-  # GET /gm_user_info
+  # GET /gm_user_infos
   def index
-    @gm_user_info = GmUserInfo.joins(:user).order('users.firstname, effective_on')
+    @gm_user_infos = GmUserInfo.joins(:user).order('users.firstname, effective_on')
   end
 
-  # GET /gm_user_info/1
+  # GET /gm_user_infos/1
   def show
   end
 
-  # GET /gm_user_info/new
+  # GET /gm_user_infos/new
   def new
     @gm_user_info = GmUserInfo.new
   end
 
-  # GET /gm_user_info/1/edit
+  # GET /gm_user_infos/1/edit
   def edit
   end
 
-  # POST /gm_user_info
+  # POST /gm_user_infos
   def create
     @gm_user_info = GmUserInfo.new(gm_user_info_params)
 
@@ -32,7 +32,7 @@ class GmUserInfoController < GmApplicationController
     end
   end
 
-  # PATCH/PUT /gm_user_info/1
+  # PATCH/PUT /gm_user_infos/1
   def update
     if @gm_user_info.update(gm_user_info_params)
       redirect_to @gm_user_info, notice: 'GM user record was successfully updated.'
@@ -41,10 +41,10 @@ class GmUserInfoController < GmApplicationController
     end
   end
 
-  # DELETE /gm_user_info/1
+  # DELETE /gm_user_infos/1
   def destroy
     @gm_user_info.destroy
-    redirect_to gm_user_info_url, notice: 'GM user record was successfully destroyed.'
+    redirect_to gm_user_infos_url, notice: 'GM user record was successfully destroyed.'
   end
 
   private
