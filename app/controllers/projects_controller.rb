a22212e438a8a379aa404da07d02c86c8820241e
Adam Konner <linesarefuzzy@gmail.com>
2016-04-28 22:32:25 -0400

    begin projects views/controllers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,24 +11,24 @@ class ProjectsController < ApplicationController
   end
 
   # GET /projects/new
-  def new
-    @project = Project.new
-  end
+  # def new
+  #   @project = Project.new
+  # end
 
   # GET /projects/1/edit
   def edit
   end
 
   # POST /projects
-  def create
-    @project = Project.new(project_params)
+  # def create
+  #   @project = Project.new(project_params)
 
-    if @project.save
-      redirect_to @project, notice: 'Project was successfully created.'
-    else
-      render :new
-    end
-  end
+  #   if @project.save
+  #     redirect_to @project, notice: 'Project was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
 
   # PATCH/PUT /projects/1
   def update
@@ -40,10 +40,10 @@ class ProjectsController < ApplicationController
   end
 
   # DELETE /projects/1
-  def destroy
-    @project.destroy
-    redirect_to projects_url, notice: 'Project was successfully destroyed.'
-  end
+  # def destroy
+  #   @project.destroy
+  #   redirect_to projects_url, notice: 'Project was successfully destroyed.'
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
