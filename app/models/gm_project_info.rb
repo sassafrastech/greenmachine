234f6e8e1e79f0d0120b6b3069ca09d51e1bdf04
Adam Konner <linesarefuzzy@gmail.com>
2017-12-01 11:24:54 -0500

    Bug with nil rates

--- app/models/gm_project_info.rb
+++ app/models/gm_project_info.rb
@@ -2,7 +2,7 @@ class GmProjectInfo < ActiveRecord::Base
   belongs_to :project
   delegate :name, to: :project
 
-  # Gets the full rate for this project over the given interval.
+  # Gets the full rate for this project over the given interval. May return nil.
   def gm_full_rate(interval)
     @gm_full_rate ||= {}
     return @gm_full_rate[interval] if @gm_full_rate[interval]
