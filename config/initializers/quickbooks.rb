3ebd74f53b3c5344fbf61ebfacf352ab09894c81
Kevin Cooper <cooperka92@gmail.com>
2020-02-13 09:34:03 -0800

    9819: Copy quickbooks initializer from madeline

---
+++ config/initializers/quickbooks.rb
@@ -0,0 +1,11 @@
+case ENV["QB_SANDBOX_MODE"]
+when "1"
+  Quickbooks.sandbox_mode = true
+when "0"
+  Quickbooks.sandbox_mode = false
+else
+  Quickbooks.sandbox_mode = !Rails.env.production?
+end
+
+Quickbooks.logger = ActiveSupport::Logger.new Rails.root.join("log", "quickbooks_gem_#{Rails.env}.log")
+Quickbooks.log = true
