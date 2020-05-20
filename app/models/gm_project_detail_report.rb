9213b85c263cdf2cc31c2a55144942fe8e468d8a
Kevin Cooper <cooperka92@gmail.com>
2020-05-20 15:05:27 -0700

    10369: Only include billed hours on invoices

--- app/models/gm_project_detail_report.rb
+++ app/models/gm_project_detail_report.rb
@@ -18,13 +18,13 @@ class GmProjectDetailReport
   def to_csv
     CSV.generate do |csv|
       # Header row.
-      csv << ['Source', 'Issue', 'Hours']
+      csv << ['Source', 'Issue', 'Billed Hours']
 
       chunk_groups.each do |user, chunks|
         csv << [user == :sassy ? 'Sassafras' : user.name]
         chunks.each do |chunk|
           csv << ['', "#{chunk.issue.tracker.name} ##{chunk.issue.id}: #{chunk.issue.subject}",
-            chunk.rounded_hours]
+            chunk.rounded_billed_hours]
         end
         csv << ['', "Total", totals[user]]
       end
@@ -40,7 +40,7 @@ class GmProjectDetailReport
   # Run main SQL query to get hours per worker in the given time period.
   def generate_chunk_data
     query = TimeEntry.
-      select('user_id, issue_id, SUM(hours) AS hours').
+      select('user_id, issue_id, activity_id, SUM(hours) AS hours').
       joins(:issue, :user).
       where(project_id: project.id).
       where('spent_on >= ?', interval.start).
@@ -75,25 +75,28 @@ class GmProjectDetailReport
 
       # A subset is a set of (issue, user, hours) tuples. We want to aggregate by issue, summing the hours.
       subset.group_by(&:issue).each do |issue, datums|
-
         hours = datums.map do |d|
-
+          is_unbilled = d.activity_id == GmChunk::UNBILLED_ACTIVITY_ID
           adjustment_factor = if subset_rate == 0
-            0 # Don't div by 0.
+                                0 # Don't div by 0.
 
-          # If user has a special issue rate, compare that against the subset rate.
-          elsif user_issue_rate = d.user.gm_issue_rate(issue, interval).try(:val)
-            user_issue_rate / subset_rate
+                              # Ignore unbilled for this report.
+                              elsif is_unbilled
+                                0
 
-          # Otherwise if this is sassy, factor is user project rate compared to full rate.
-          # (For contractors, the subset_rate /is/ the project rate)
-          elsif user == :sassy
-            d.user.gm_project_rate(project, interval).val / subset_rate
+                              # If user has a special issue rate, compare that against the subset rate.
+                              elsif user_issue_rate = d.user.gm_issue_rate(issue, interval).try(:val)
+                                user_issue_rate / subset_rate
 
-          # Otherwise there is no need to adjust.
-          else
-            1
-          end
+                              # Otherwise if this is sassy, factor is user project rate compared to full rate.
+                              # (For contractors, the subset_rate /is/ the project rate)
+                              elsif user == :sassy
+                                d.user.gm_project_rate(project, interval).val / subset_rate
+
+                              # Otherwise there is no need to adjust.
+                              else
+                                1
+                              end
 
           d.hours * adjustment_factor
         end.sum
@@ -105,6 +108,6 @@ class GmProjectDetailReport
   end
 
   def calculate_totals
-    self.totals = Hash[*chunk_groups.map{ |u, chunks| [u, chunks.sum(&:rounded_hours)] }.flatten]
+    self.totals = Hash[*chunk_groups.map{ |u, chunks| [u, chunks.sum(&:rounded_billed_hours)] }.flatten]
   end
 end
